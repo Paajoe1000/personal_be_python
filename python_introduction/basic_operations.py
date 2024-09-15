fd52cb556fd3b901f0a6f85a08d25e6a23f4fe41
paajoe100 <kojoyankson12@gmail.com>
2024-09-15 22:14:23 +0100

    Add numbers result and print

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+result_sum = number1 + number2
+result_difference = number1 - number2
+result_product = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {result_sum}")
+print(f"Subtraction of {number2} from {number1} is {result_difference}")
+print(f"Multiplication of {number1} and {number2} is {result_product}")
